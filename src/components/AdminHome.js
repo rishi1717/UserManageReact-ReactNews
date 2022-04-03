54ecf996ff2f7eec6de94d75d08753e3af380d48
rishi1717 <rshrrrss@gmail.com>
2022-04-03 15:21:19 +0530

    admin ui basics completed

--- src/components/AdminHome.js
+++ src/components/AdminHome.js
@@ -1,8 +1,28 @@
 import React from "react"
 import UserData from "./subComponents/UserData"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
-import { Box, Button, Container, TextField } from "@mui/material"
+import {
+	Box,
+	Button,
+	Container,
+	Modal,
+	TextField,
+} from "@mui/material"
 import Navbar from "./Navbar"
+
+const style = {
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	maxWidth: 500,
+	minWidth: 250,
+	bgcolor: "background.paper",
+	color: "white",
+	border: "2px solid #000",
+	boxShadow: 24,
+	p: 4,
+}
 
 const darkTheme = createTheme({
 	palette: {
@@ -17,9 +37,13 @@ const handleSearch = (event) => {
 }
 
 function AdminHome() {
+	const [open, setOpen] = React.useState(false)
+	const handleOpen = () => setOpen(true)
+	const handleClose = () => setOpen(false)
+
 	return (
 		<>
-			<Navbar adminNav={true}/>
+			<Navbar adminNav={true} />
 			<ThemeProvider theme={darkTheme}>
 				<Container maxWidth="md">
 					<Box
@@ -50,9 +74,68 @@ function AdminHome() {
 						noValidate
 						sx={{ mt: 1, mb: 2 }}
 					>
-						<Button variant="outlined" sx={{ mt: 5 }}>
+						<Button
+							variant="outlined"
+							sx={{ mt: 5 }}
+							onClick={handleOpen}
+						>
 							Add user
 						</Button>
+						<Modal
+							keepMounted
+							open={open}
+							onClose={handleClose}
+							aria-labelledby="keep-mounted-modal-title"
+							aria-describedby="keep-mounted-modal-description"
+						>
+							<Box component="form" onSubmit={""} noValidate sx={style}>
+								<TextField
+									margin="normal"
+									required
+									fullWidth
+									id="name"
+									label="Name"
+									name="name"
+									autoFocus
+									variant="standard"
+								/>
+								<TextField
+									margin="normal"
+									required
+									fullWidth
+									id="email"
+									label="Email Address"
+									name="email"
+									variant="standard"
+								/>
+								<TextField
+									margin="normal"
+									fullWidth
+									id="Phone"
+									label="Phone"
+									name="Phone"
+									variant="standard"
+								/>
+								<TextField
+									margin="normal"
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									variant="standard"
+								/>
+								<Button
+									type="submit"
+									fullWidth
+									variant="outlined"
+									sx={{ mt: 3, mb: 2 }}
+								>
+									Add
+								</Button>
+							</Box>
+						</Modal>
 					</Box>
 				</Container>
 			</ThemeProvider>
--- src/components/subComponents/UserData.js
+++ src/components/subComponents/UserData.js
@@ -4,9 +4,28 @@ import AccordionSummary from "@mui/material/AccordionSummary"
 import AccordionDetails from "@mui/material/AccordionDetails"
 import Typography from "@mui/material/Typography"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import { Button, Container } from "@mui/material"
+import { Button, Container, TextField } from "@mui/material"
+import Box from "@mui/material/Box"
+import Modal from "@mui/material/Modal"
+
+const style = {
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	minWidth: 250,
+	maxWidth: 500,
+	bgcolor: "background.paper",
+	color: "white",
+	border: "2px solid #000",
+	boxShadow: 24,
+	p: 4,
+}
 
 export default function UserData() {
+	const [open, setOpen] = React.useState(false)
+	const handleOpen = () => setOpen(true)
+	const handleClose = () => setOpen(false)
 	let arr = [1, 2, 3, 4, 5]
 	return (
 		<Container>
@@ -26,14 +45,73 @@ export default function UserData() {
 							<Typography>email: {elem}</Typography>
 							<Container>
 								<Button
-									sx={{ float: "right", marginLeft:'0.4rem', marginBottom:'0.5rem' }}
+									sx={{
+										float: "right",
+										marginLeft: "0.4rem",
+										marginBottom: "0.5rem",
+									}}
 									size="small"
 									variant="outlined"
+									onClick={handleOpen}
 								>
 									Update
 								</Button>
+								<Modal
+									// keepMounted
+									open={open}
+									onClose={handleClose}
+									aria-labelledby="keep-mounted-modal-title"
+									aria-describedby="keep-mounted-modal-description"
+								>
+									<Box
+										component="form"
+										onSubmit={""}
+										noValidate
+										sx={style}
+									>
+										<TextField
+											margin="normal"
+											required
+											fullWidth
+											id="name"
+											label="Name"
+											name="name"
+											autoFocus
+											variant="standard"
+										/>
+										<TextField
+											margin="normal"
+											required
+											fullWidth
+											id="email"
+											label="Email Address"
+											name="email"
+											variant="standard"
+										/>
+										<TextField
+											margin="normal"
+											fullWidth
+											id="Phone"
+											label="Phone"
+											name="Phone"
+											variant="standard"
+										/>
+										<Button
+											type="submit"
+											fullWidth
+											variant="outlined"
+											sx={{ mt: 3, mb: 2 }}
+										>
+											Update
+										</Button>
+									</Box>
+								</Modal>
 								<Button
-									sx={{ float: "right", marginLeft:'0.4rem', marginBottom:'0.5rem' }}
+									sx={{
+										float: "right",
+										marginLeft: "0.4rem",
+										marginBottom: "0.5rem",
+									}}
 									size="small"
 									variant="outlined"
 									color="error"
